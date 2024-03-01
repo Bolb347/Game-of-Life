cb678df292a3cd742d9801b38272c87ca4abaf59
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-03-01 14:46:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function drawLiveTile(x, y) {
 function drawTiles() {
     for (let x = 0; x < map_size; x ++) {
         for (let y = 0; y < map_size; y ++) {
-            if (tile_array[x][y]) {
+            if (tile_array[x] && tile_array[x][y]) {
                 drawLiveTile(x * tile_size, y * tile_size);
             }
         }
@@ -31,41 +31,38 @@ function drawTiles() {
 
 function testNeighbors(x, y) {
     let neighbor_count = 0
-    if (tile_array[x + 1][y]) {
+    if (tile_array[x + 1] && tile_array[x + 1][y]) {
         neighbor_count ++;
     }
-    if (tile_array[x + 1][y + 1]) {
+    if (tile_array[x + 1] && tile_array[x + 1][y + 1]) {
         neighbor_count ++;
     }
-    if (tile_array[x + 1][y - 1]) {
+    if (tile_array[x + 1] && tile_array[x + 1][y - 1]) {
         neighbor_count ++;
     }
-    if (tile_array[x][y + 1]) {
+    if (tile_array[x] && tile_array[x][y + 1]) {
         neighbor_count ++;
     }
-    if (tile_array[x][y - 1]) {
+    if (tile_array[x] && tile_array[x][y - 1]) {
         neighbor_count ++;
     }
-    if (tile_array[x - 1][y]) {
+    if (tile_array[x - 1] && tile_array[x - 1][y]) {
         neighbor_count ++;
     }
-    if (tile_array[x - 1][y + 1]) {
+    if (tile_array[x - 1] && tile_array[x - 1][y + 1]) {
         neighbor_count ++;
     }
-    if (tile_array[x - 1][y - 1]) {
+    if (tile_array[x - 1] && tile_array[x - 1][y - 1]) {
         neighbor_count ++;
     }
     if (tile_array[x][y]) {
         if (neighbor_count < 2) {
             tile_array[x][y] = 0;
         }
-        if (neighbor_count === 2 || neighbor_count === 3) {
-            tile_array[x][y] = 1;
-        }
-        if (neighbor_count < 3) {
+        if (neighbor_count > 3) {
             tile_array[x][y] = 0;
         }
-    } else if (tile_array[x][y] === 0) {
+    } else {
         if (neighbor_count === 3) {
             tile_array[x][y] = 1;
         }
@@ -75,10 +72,10 @@ function testNeighbors(x, y) {
 function generateBlankArray(size) {
     blank = []
     for (let r = 0; r < size; r ++) {
-        blank.push(r % 2);
+        blank.push(Math.random()*0.7);
     }
     for (let r = 0; r < size; r ++) {
-        tile_array.push(blank.slice(0));
+        tile_array.push(blank.slice(0).map((e) => Math.round(e * Math.random())));
     }
 }
 
@@ -98,6 +95,11 @@ async function main() {
     while (true) {
         clearCanvas();
         drawTiles();
+        for (let x = 0; x < map_size; x ++) {
+            for (let y = 0; y < map_size; y ++) {
+                testNeighbors(x, y);
+            }
+        }
         await sleep(100);
     }
 }
