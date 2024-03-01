70e1da007ebaad1a8476e76ce75c4417e498d758
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-02-29 16:50:33 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext();
+
+let tile_size = 16;
+
+function changeColor(color) {
+    context.fillStyle = color;
+}
+
+function drawTile(x, y) {
+    context.fillRect(x, y, tile_size, tile_size);
+}
+
 function main() {
 }
